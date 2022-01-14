2ea0de35668800713c374553e1be09e8e0cfbafc
Clara Villa <Clara.Villa@lego.com>
2022-01-14 18:39:13 +0000

    fixed loading images

--- js/index.js
+++ js/index.js
@@ -3,23 +3,21 @@ const ctx = canvas.getContext("2d");
 
 
 function drawBasicCanvas() {
-  ctx.fillStyle = "green";
+ ctx.fillStyle = "#ddb8b8";
   ctx.fillRect(0, 0, 1200, 85);
   ctx.fillRect(0, 310, 1200, 85);
-  ctx.fillStyle = "black"
-  ctx.fillRect(1120, 5, 70, 70);
-  // let teaPotImg = new Image();
-  // teaPotImg.src = "/images/teapot.png"
-  // teaPotImg.onload = () => {
-  // ctx.drawImage(teaPotImg, 1120, 5, 70, 70);
-  // };
+  let teaPotImg = new Image();
+  teaPotImg.src = "./images/teapot.png";
+  teaPotImg.onload = function () {
+  ctx.drawImage(teaPotImg, 1120, 5, 70, 70);
+  };
 }
 
 class Component {
-  constructor(width, height, color, x, y) {
+  constructor(width, height, img, x, y) {
     this.width = width;
     this.height = height;
-    this.color = color;
+    this.img = img;
     this.x = x;
     this.y = y;
   }
@@ -27,15 +25,15 @@ class Component {
   draw() {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height)
-    // let compImg = new Image();
-    // compImg.src = this.img;
-    // compImg.onload = () => {
-    //   ctx.drawImage(compImg, this.x, this.y, this.width, this.height);
-    // };
+    let compImg = new Image();
+    compImg.src = this.img;
+    compImg.onload = () => {
+   ctx.drawImage(compImg, this.x, this.y, this.width, this.height);
+    };
   }
 }
 
-const teaCup = new Component(60, 60, "blue", 570, 323);
+const teaCup = new Component(60, 60, "./images/teacup.png", 570, 323);
 
 
   drawBasicCanvas();
